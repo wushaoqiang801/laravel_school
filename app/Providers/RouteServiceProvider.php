300eb414dc0dbb6bc04b5ae2c23dfd6933504dc0
wushaoqiang <wushaoqiang801@gmail.com>
2021-09-22 23:35:00 +0800

    no message

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
+    /** 
      * This namespace is applied to your controller routes.
      *
      * In addition, it is set as the URL generator's root namespace.
