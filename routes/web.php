6e77cafe384d24a6f3f6af792e4877ed6fc020f2
wushaoqiang <wushaoqiang801@gmail.com>
2021-09-22 22:29:18 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('login/test','LoginController@test');
+///app/public/login/test
+Route::get('/','LoginController@test');
